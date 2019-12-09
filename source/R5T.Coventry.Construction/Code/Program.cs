1c93aa99df1e0d4a33f818e8ebfd3fbaa87f7b9c
MinexAutomation <minex.automation@gmail.com>
2019-12-08 22:47:13 -0800

    Added appsettings tests.

--- source/R5T.Coventry.Construction/Code/Program.cs
+++ source/R5T.Coventry.Construction/Code/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Derby;
+using R5T.Richmond;
+
+
 namespace R5T.Coventry.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.TryGetValue01();
+        }
+
+        private static void TryGetValue01()
+        {
+            var serviceProvider = Program.GetServiceProvider();
+
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+            var value01 = configuration["Value01"];
+
+            Console.WriteLine($"Value01: {value01}");
+        }
+
+        private static IServiceProvider GetServiceProvider()
+        {
+            var serviceProvider = ApplicationBuilder.UseStartup<Startup, ApplicationConfigurationStartup>();
+            return serviceProvider;
         }
     }
 }
---
+++ source/R5T.Coventry.Construction/Code/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+using CoventryApplicationStartupBase = R5T.Coventry.ApplicationStartupBase;
+
+
+namespace R5T.Coventry.Construction
+{
+    public class Startup : CoventryApplicationStartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+    }
+}
