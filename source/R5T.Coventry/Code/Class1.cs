be1b9936fa45e334e68babe2bf4af08860a4941e
MinexAutomation <minex.automation@gmail.com>
2019-12-08 22:07:14 -0800

    Added application startup base and configuration builder extensions.

--- source/R5T.Coventry/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Coventry
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Coventry/Code/Startup/ApplicationStartupBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using R5T.Coventry.Extensions;
+
+using RichmondApplicationStartupBase = R5T.Richmond.ApplicationStartupBase;
+
+
+namespace R5T.Coventry
+{
+    public abstract class ApplicationStartupBase : RichmondApplicationStartupBase
+    {
+        public ApplicationStartupBase(ILogger<ApplicationStartupBase> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureConfigurationBody(IConfigurationBuilder configurationBuilder, IServiceProvider configurationServicesProvider)
+        {
+            configurationBuilder
+                .AddDefaultAndConfigurationNameSpecificAppSettings(configurationServicesProvider)
+                ;
+        }
+    }
+}
