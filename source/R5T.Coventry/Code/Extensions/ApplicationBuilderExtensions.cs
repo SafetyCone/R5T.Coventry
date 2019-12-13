ea46746d43830e39d8f570218a4da233262b73cd
MinexAutomation <minex.automation@gmail.com>
2019-12-13 13:21:55 -0800

    Added application builder extension to make using an R5T.Coventry-based application startup easy.

---
+++ source/R5T.Coventry/Code/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Derby;
+using R5T.Richmond;
+
+
+namespace R5T.Coventry
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IServiceProvider UseStartupFromCoventryWithDerbyConfigurationStartup<TStartup>(this ApplicationBuilder applicationBuilder)
+            where TStartup: ApplicationStartupBase
+        {
+            var serviceProvider = applicationBuilder.UseStartupWithDerbyConfigurationStartup<TStartup>();
+            return serviceProvider;
+        }
+    }
+}
