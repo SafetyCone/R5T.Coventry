10908fa1bdee66f989792c10ecfe37c20660f8ee
MinexAutomation <minex.automation@gmail.com>
2019-12-14 01:43:02 -0800

    Added a forwarding class for R5T.Richmond.ApplicationBuilder in the R5T.Coventry namespace so that Coventry clients would not have to use both namespaces (only R5T.Coventry).

---
+++ source/R5T.Coventry/Code/Classes/ApplicationBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RichmondApplicationBuilder = R5T.Richmond.ApplicationBuilder;
+
+
+namespace R5T.Coventry
+{
+    public static class ApplicationBuilder
+    {
+        public static RichmondApplicationBuilder New()
+        {
+            var applicationBuilder = RichmondApplicationBuilder.New();
+            return applicationBuilder;
+        }
+    }
+}
