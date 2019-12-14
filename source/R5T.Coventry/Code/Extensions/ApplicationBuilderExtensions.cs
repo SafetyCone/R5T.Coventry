4cd03013ffa50a3a5e728ca565f42379e1ae0d38
MinexAutomation <minex.automation@gmail.com>
2019-12-14 01:43:47 -0800

    Had to alias Richmond ApplicationBuilder class.

--- source/R5T.Coventry/Code/Extensions/ApplicationBuilderExtensions.cs
+++ source/R5T.Coventry/Code/Extensions/ApplicationBuilderExtensions.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 
 using R5T.Derby;
-using R5T.Richmond;
+using RichmondApplicationBuilder = R5T.Richmond.ApplicationBuilder;
 
 
 namespace R5T.Coventry
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IServiceProvider UseStartupFromCoventryWithDerbyConfigurationStartup<TStartup>(this ApplicationBuilder applicationBuilder)
+        public static IServiceProvider UseStartupFromCoventryWithDerbyConfigurationStartup<TStartup>(this RichmondApplicationBuilder applicationBuilder)
             where TStartup: ApplicationStartupBase
         {
             var serviceProvider = applicationBuilder.UseStartupWithDerbyConfigurationStartup<TStartup>();
