299a844849bd930a22fd91b385555f0f851bbd86
MinexAutomation <minex.automation@gmail.com>
2019-12-21 23:43:38 -0800

    Added IConfigurationNameProvider service to the opinionated application startup.

--- source/R5T.Coventry/Code/Startup/ApplicationStartupBase.cs
+++ source/R5T.Coventry/Code/Startup/ApplicationStartupBase.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using R5T.Derby.Extensions;
+using R5T.Ives;
+using R5T.Ives.Configuration;
 
 using RichmondApplicationStartupBase = R5T.Richmond.ApplicationStartupBase;
 
@@ -29,14 +31,17 @@ namespace R5T.Coventry
         }
 
         /// <summary>
-        /// Adds console logging.
+        /// Adds console logging and <see cref="IConfigurationNameProvider"/>.
         /// </summary>
         protected override void ConfigureServicesBody(IServiceCollection services)
         {
-            services.AddLogging(loggingBuilder =>
-            {
-                loggingBuilder.AddConsole();
-            });
+            services
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddConsole();
+                })
+                .AddSingleton<IConfigurationNameProvider, DirectConfigurationBasedConfigurationNameProvider>()
+                ;
         }
     }
 }
