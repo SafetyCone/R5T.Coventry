72269cd6370f2d94b43ba1384c4bad42ba787d94
MinexAutomation <minex.automation@gmail.com>
2019-12-26 18:32:48 -0800

    Added documentation.

--- source/R5T.Coventry/Code/Classes/ApplicationBuilder.cs
+++ source/R5T.Coventry/Code/Classes/ApplicationBuilder.cs
@@ -5,6 +5,10 @@ using RichmondApplicationBuilder = R5T.Richmond.ApplicationBuilder;
 
 namespace R5T.Coventry
 {
+    /// <summary>
+    /// A type in the <see cref="R5T.Coventry"/> namespace that forwards the <see cref="R5T.Richmond.ApplicationBuilder"/> type.
+    /// This avoids an extra using statement for the <see cref="R5T.Richmond"/> namespace.
+    /// </summary>
     public static class ApplicationBuilder
     {
         public static RichmondApplicationBuilder New()
