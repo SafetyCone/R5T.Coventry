783f563c418a7bef4963e2a1052f6593967b70da
MinexAutomation <minex.automation@gmail.com>
2020-06-08 15:23:17 -0700

    Follow-through on moving configuration-specific appsettings.json file extensions from Derby to base Shrewsbury library.

--- source/R5T.Coventry/Code/Startup/ApplicationStartupBase.cs
+++ source/R5T.Coventry/Code/Startup/ApplicationStartupBase.cs
@@ -4,9 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using R5T.Derby.Extensions;
 using R5T.Ives;
 using R5T.Ives.Configuration;
+using R5T.Shrewsbury.Extensions;
 
 using RichmondApplicationStartupBase = R5T.Richmond.ApplicationStartupBase;
 
@@ -29,7 +29,7 @@ namespace R5T.Coventry
         protected override void ConfigureConfigurationBody(IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
         {
             configurationBuilder
-                .AddDefaultAndConfigurationSpecificAppSettingsJsonFiles(configurationServiceProvider, true) // Make the configuration-name-specific appsettings file optional since all configuration might just be in the default appsettings file.
+                .AddDefaultAndConfigurationSpecificAppSettingsJsonFiles(configurationServiceProvider)
                 ;
         }
 
