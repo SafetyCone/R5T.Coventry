c9358e169fde389d37587fc90690a529decd7d2b
MinexAutomation <minex.automation@gmail.com>
2020-06-10 17:43:25 -0700

    Added default dervied classes for the abstract StartupConfigurationBase and StartupConfigurationConfiguration base classes.

---
+++ source/R5T.Coventry/Code/Startup/Classes/DefaultStartupConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Liverpool;
+
+
+namespace R5T.Coventry
+{
+    /// <summary>
+    /// Default startup configuration type.
+    /// </summary>
+    public class DefaultStartupConfiguration : StartupConfigurationBase
+    {
+        #region Static
+
+        public static ServiceProvider GetServiceProvider()
+        {
+            using (var configurationConfigurationServiceProvider = ServiceProviderServiceBuilder.New().UseStartupAndBuild<DefaultStartupConfigurationConfiguration>())
+            {
+                var configurationServiceProvider = ServiceProviderServiceBuilder.New().UseStartupAndBuild<DefaultStartupConfiguration>(configurationConfigurationServiceProvider);
+                return configurationServiceProvider;
+            }
+        }
+
+        #endregion
+
+
+        public DefaultStartupConfiguration(ILogger<DefaultStartupConfiguration> logger)
+            : base(logger)
+        {
+        }
+    }
+}
---
+++ source/R5T.Coventry/Code/Startup/Classes/DefaultStartupConfigurationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.Coventry
+{
+    /// <summary>
+    /// Default startup configuration configuration.
+    /// </summary>
+    public class DefaultStartupConfigurationConfiguration : StartupConfigurationConfigurationBase
+    {
+        public DefaultStartupConfigurationConfiguration(ILogger<DefaultStartupConfigurationConfiguration> logger)
+            : base(logger)
+        {
+        }
+    }
+}
